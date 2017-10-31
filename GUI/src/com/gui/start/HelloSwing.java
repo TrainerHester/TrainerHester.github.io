8b15ee0f1b338cc8f4f8848e064273a1149265f7
PC201504192016\Administrator <hesternanweiguo@gmail.com>
2017-10-31 19:31:35 +0800

    gui-start

---
+++ GUI/src/com/gui/start/HelloSwing.java
@@ -0,0 +1,12 @@
+package com.gui.start;
+
+import javax.swing.*;
+
+public class HelloSwing {
+	public static void main(String [] args) {
+		JFrame frame = new JFrame("Hello Swing");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 160);
+		frame.setVisible(true);
+	}
+}
